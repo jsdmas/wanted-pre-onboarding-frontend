18bef3514591d6caa4c585672dcad84b3f877cb8
jsdmas <sdmas5730@gmail.com>
2023-06-02 15:14:19 +0900

    feat : Assignment(1) register regex

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -1,27 +1,92 @@
 import styled from "styled-components";
+import { useCallback, useEffect, useState } from "react";
+
+function SignUp() {
+    const [values, setValues] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({ email: "", password: "" });
+    const [isBlur, setIsBlur] = useState({ email: false, password: false });
+    const [disabled, setDisabled] = useState(true);
+
+    const onSubmit = (event: React.BaseSyntheticEvent) => {
+        event.preventDefault();
+        alert(JSON.stringify(values));
+    };
+
+    const vaildate = useCallback(() => {
+        const errors = {
+            email: "",
+            password: ""
+        };
+
+        if (!values.email.includes("@")) {
+            setDisabled(true);
+            errors.email = "'@'를 포함해주세요.";
+        }
+        if (!(values.password.length >= 8)) {
+            setDisabled(true);
+            errors.password = "비밀번호는 8자 이상 입력해주세요.";
+        }
+
+        if (!Object.values(errors).some(value => value)) setDisabled(false);
+
+        return errors;
+    }, [values]);
+
+    // 포커스 아웃
+    const onBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+        setIsBlur({
+            ...isBlur,
+            [event.target.name]: true
+        });
+    };
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        });
+    };
+
+
+    useEffect(() => {
+        const errors = vaildate();
+        setErrors(errors);
+    }, [values, vaildate]);
+
+    return (
+        <Wrapper>
+            <h1>회원가입</h1>
+            <RegisterForm onSubmit={onSubmit} name="register">
+                <input data-testid="email-input" name="email" onBlur={onBlur} onChange={onChange} placeholder="email" />
+                <span>{isBlur.email && errors.email ? errors.email : null}</span>
+                <input data-testid="password-input" name="password" onBlur={onBlur} onChange={onChange} placeholder="비밀번호는 8자 이상입니다." />
+                <span>{isBlur.password && errors.password ? errors.password : null}</span>
+                <button data-testid="signup-button" type="submit" disabled={disabled}>회원가입</button>
+            </RegisterForm>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.main`
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
 
 const RegisterForm = styled.form`
     display: flex;
     flex-direction: column;
     width: 300px;
-    height: 100px;
-    justify-content: space-between;
+    height: 150px;
+    justify-content: space-around;
+    input{
+        height: 30px;
+    }
+    span{
+        height: 15px;
+    }
 `;
 
-function SignUp() {
-    const onSubmit = (event: React.BaseSyntheticEvent) => {
-        event.preventDefault();
-
-    };
-    return (
-        <>
-            <h1>회원가입</h1>
-            <RegisterForm onSubmit={onSubmit} name="register">
-                <input data-testid="email-input" type="email" />
-                <input data-testid="password-input" type="password" />
-                <button data-testid="signup-button" type="submit">회원가입</button>
-            </RegisterForm>
-        </>
-    );
-};
 export default SignUp;
