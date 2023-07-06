a724fe567d645fd2740dfe4fa318952f3eb9ba0c
jsdmas <sdmas5730@gmail.com>
2023-07-06 16:56:15 +0900

    :seedling: Chore : init eslint & prettier & craco & tsconfig

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
 # wanted 사전과제
 
-- 지원자 이름 : 장진호
-- 프로젝트 실행 방법
-
-```
-npm start
-```
-
-- 배포링크 : [https://jsdmas.github.io/wanted-pre-onboarding-frontend/](https://jsdmas.github.io/wanted-pre-onboarding-frontend/)
-
-- 사용 라이브러리
-  - styled-components, react-rotuer
-  - 언어 : typeSciprt
-  - 배포 : gh-pages
-
 # 23.7.6 refactore
--- src/api/signApi.ts
+++ src/api/signApi.ts
@@ -1,34 +1,34 @@
-import { IinitalValues } from "../types/user";
+import { IinitalValues } from '../types/user';
 
-const API_ADDRESS = "https://www.pre-onboarding-selection-task.shop";
-const API_SIGNUP_URL = "auth/signup";
-const API_SIGNIN_URL = "auth/signin";
+const API_ADDRESS = 'https://www.pre-onboarding-selection-task.shop';
+const API_SIGNUP_URL = 'auth/signup';
+const API_SIGNIN_URL = 'auth/signin';
 
 export const signUpApi = async (values: IinitalValues) => {
-    const response = await fetch(`${API_ADDRESS}/${API_SIGNUP_URL}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-    });
-    if (!response.ok) {
-        throw new Error("회원가입 실패!");
-    }
-    return response;
+  const response = await fetch(`${API_ADDRESS}/${API_SIGNUP_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(values),
+  });
+  if (!response.ok) {
+    throw new Error('회원가입 실패!');
+  }
+  return response;
 };
 
 export const signInApi = async (values: IinitalValues) => {
-    const response = await fetch(`${API_ADDRESS}/${API_SIGNIN_URL}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-    });
-    if (!response.ok) {
-        throw new Error("로그인 실패!");
-    }
-    const responseData = await response.json();
-    return responseData;
+  const response = await fetch(`${API_ADDRESS}/${API_SIGNIN_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(values),
+  });
+  if (!response.ok) {
+    throw new Error('로그인 실패!');
+  }
+  const responseData = await response.json();
+  return responseData;
 };
