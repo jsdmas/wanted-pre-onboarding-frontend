f4efdbc7edcce0a2128a15a4744fca396a879497
jsdmas <sdmas5730@gmail.com>
2023-07-07 12:16:45 +0900

    :sparkles: Feat : redirect Router config

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,20 +1,39 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, Navigate, Outlet } from 'react-router-dom';
 import SignUp from './routes/SignUp';
 import SignIn from './routes/SignIn';
 import ToDo from './routes/ToDo';
 import Home from './routes/Home';
+import { getToken } from './utils/token';
+import { TOKEN_KEY } from './constants/auth';
+import { PATH } from './constants/path';
 
 function Router() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route element={<Home />} path="/" />
-        <Route element={<SignUp />} path="/signup" />
-        <Route element={<SignIn />} path="/signin" />
-        <Route element={<ToDo />} path="/todo" />
+        <Route element={<PublicRouter />}>
+          <Route element={<SignUp />} path="/signup" />
+          <Route element={<SignIn />} path="/signin" />
+        </Route>
+        <Route element={<PrivateRouter />}>
+          <Route element={<ToDo />} path="/todo" />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default Router;
+
+const PrivateRouter = () => {
+  const token = getToken(TOKEN_KEY);
+  return <Outlet />;
+};
+
+const PublicRouter = () => {
+  const token = getToken(TOKEN_KEY);
+  // replace : 뒤로가기 금지
+  // navigate : redirect
+  return token ? <Navigate to={PATH.TODO} replace /> : <Outlet />;
+};
--- src/api/instance.ts
+++ src/api/instance.ts
@@ -9,7 +9,9 @@ export const instance = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
+// interceptors : then | catch로 처리되기 전에 요청과 응답을 가로챌수 있다.
 instance.interceptors.request.use((config) => {
+  // 요청을 보내기 전 수행할 로직 (에러 발생시 실행될 로직도 설정 가능)
   const token = getToken(TOKEN_KEY);
 
   config.headers['Authorization'] = token ? `Bearer ${token}` : '';
